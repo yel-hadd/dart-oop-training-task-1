73405988663d741a5b0849a69296a14d7163a133
Yassine El Haddad <megahaddad15@gmail.com>
2023-02-03 16:44:01 +0100

    add mobile.dart

---
+++ Mobile.dart
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Mobile.dart                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yel-hadd <yel-hadd@mail.com>               +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/03 16:14:26 by yel-hadd          #+#    #+#             */
+/*   Updated: 2023/02/03 16:22:45 by yel-hadd         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+class Mobile
+{
+	String brand;
+	String model;
+	double mrp;
+	double discount;
+
+	Mobile (this.brand, this.model, this.mrp, this.discount);
+	
+  double getActualPrice()
+	{
+		return (mrp - ((mrp*discount)/100));
+	}
+	void printDetails()
+	{
+		print("Brand : $brand");
+		print("Model : $model");
+		print("MRP : $mrp");
+		print("Discount : $discount");
+		print("Discount Price : ${getActualPrice()}");
+	}
+}
